8b2d2c28160a957c46c54b9de773470bb0c7fcbe
Kangjunesu <kulbit92@naver.com>
2023-08-19 14:22:10 +0900

    메인 스레드, 작업스레드 분리

---
+++ src/BeepTask.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+
+public class BeepTask implements Runnable { // 작업 스레드 : 비프음
+    @Override
+    public void run() {
+        Toolkit toolkit = Toolkit.getDefaultToolkit(); // Toolkit 객체 얻기
+
+        for (int i = 0; i < 5; i++) {
+            toolkit.beep();
+            try {
+                Thread.sleep(500);
+            } catch (Exception e) {
+            }
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,17 @@
 import java.awt.*;
 
-public class Main {
-    public static void main(String[] args) {
-        Toolkit toolkit = Toolkit.getDefaultToolkit(); // Toolkit 객체 얻기
+public class Main {  // 메인 스레드 : 출력
 
-        for(int i =0; i<5; i++){
-            toolkit.beep();
-            try{Thread.sleep(500); } catch(Exception e) {}
-        }
+    public static void main(String[] args) {
+      Runnable beepTask = new BeepTask();
+      Thread thread = new Thread(beepTask);
+      thread.start();
 
         for(int i =0; i<5; i++){
             System.out.println("띵");
             try{Thread.sleep(500); } catch(Exception e) {}
         }
 
-        
     }
 }
+
